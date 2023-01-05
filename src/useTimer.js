2bf01c99d4504e78acc3edb31be8afca144921f0
Anh Ky Nguyen <anh163@gmail.com>
2023-01-05 22:00:20 +0700

    Solution

--- src/useTimer.js
+++ src/useTimer.js
@@ -1,21 +1,30 @@
 import { useState, useRef } from "react";
 
 const useTimer = (ini = 0) => {
-  const [time, setTime] = "Your code here";
+  const [time, setTime] = useState(0);
 
-  const isStart = "Your code here";
-  const active = "Your code here";
-  const refInterval = "Your code here";
+  const isStart = useRef(false);
+  const active = useRef();
+  const refInterval = useRef(0);
 
   const startTimer = () => {
-    "Your code here";
+    isStart.current = true;
     active.current.disabled = true;
+    refInterval.current = setInterval(() => {
+      if (isStart.current) {
+        setTime((time) => time + 1);
+      }
+    }, 1000);
   };
+
   const stopTimer = () => {
-    "Your code here";
+    isStart.current = false;
+    clearInterval(refInterval.current);
+    active.current.disabled = false;
   };
   const resetTimer = () => {
-    "Your code here";
+    setTime(0);
+    isStart.current = false;
     active.current.disabled = false;
   };
 
